5b1a02475f4e9fca9bc2418fa28a3d1db0031f29
WorriedSeat <vanish_for.white@mail.ru>
2026-03-17 00:25:14 +0300

    reworking index creation

--- src/dataset/index.py
+++ src/dataset/index.py
@@ -1,92 +1,125 @@
-import os
-import yaml, pickle
-from tqdm import tqdm
+import os, argparse
+import torch
 import h5py
 import faiss
+import yaml, pickle
 import numpy as np
 import pandas as pd
-import argparse
+from tqdm import tqdm
 from sentence_transformers import SentenceTransformer
+
+def _load_config():
+    if os.path.exists("config/config.yaml"):
+        with open("config/config.yaml", "r") as f:
+            return yaml.safe_load(f)
+    
+    else:
+        raise FileNotFoundError("ERROR: config not found at config/config.yaml .\n \
+                                ensure you run index.py from project's root directory")
+
+def _create_save_metadata() -> tuple[list, list]:
+    config = _load_config()
+    DATA_PREP_PATH = config["paths"]["film_data"]
+    METADATA_PATH = config["paths"]["faiss_metadata"]
+    
+    #Check if the preprocessed data is present
+    if not os.path.exists(DATA_PREP_PATH):
+        raise FileNotFoundError(f"ERROR: No preprocessed data found!\n  Run src/dataset/data_proc.py auto firstly.")
+        
+    data = pd.read_csv(DATA_PREP_PATH, usecols=["title", "title_plot", "title_meta"])
+    # data = data.iloc[:300] #XXX for test_embeddings_build
+    data.reset_index(drop=True, inplace=True)
+
+    #Creating text & metadata lists for index
+    texts = []
+    metadata = []
+    for idx, row in data.iterrows():
+        #Meta chunk
+        texts.append(row["title_meta"])
+        metadata.append({"row_idx": idx, "chunk_type": "meta", "title": row["title"], "chunk_text": row["title_meta"]})
+        
+        #Plot chunk
+        texts.append(row["title_plot"])
+        metadata.append({"row_idx": idx, "chunk_type": "plot", "title": row["title"], "chunk_text": row["title_plot"]})
+
+    #Saving metadata
+    with open(METADATA_PATH, 'wb') as f: 
+        pickle.dump(metadata, f)
+    print(f"Saved metadata {METADATA_PATH}")
+
+    return texts, metadata
+
+def _create_embeddings(embed_model:SentenceTransformer):
+    print("Creating embeddings:")
+    
+    config = _load_config()
+    EMBED_PATH = config["paths"]["embeddings"]
+    EMBED_DIM = embed_model.get_sentence_embedding_dimension()
+    
+    texts, _ = _create_save_metadata()
+    
+    # Batch encode and append to H5
+    batch_size = 128
+    with h5py.File(EMBED_PATH, 'w') as hf:
+        dset = hf.create_dataset("embeddings", shape=(len(texts), EMBED_DIM), dtype='float32', chunks=True)
+        for i in tqdm(range(0, len(texts), batch_size)):
+            batch = texts[i:i + batch_size]
+            batch_emb = embed_model.encode(batch, batch_size=batch_size, show_progress_bar=False, precision='float32', normalize_embeddings=True)
+            dset[i:i + len(batch_emb)] = batch_emb
+            print(f"Batch {i//batch_size + 1} saved")
+    
+    print(f"Embeddings saved to {EMBED_PATH}")
 
 class FaissIndex:
     def __init__(self):
-        with open("config/config.yaml", "r") as f:
-            self.config = yaml.safe_load(f)
-                
-        self.embed_model = SentenceTransformer(self.config["models"]["embedding_model"])
+        config = _load_config()
+        
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.embed_model = SentenceTransformer(config["models"]["embedding_model"], device=self.device)
         self.embed_size = self.embed_model.get_sentence_embedding_dimension() #768
         self.max_seq_length = self.embed_model.get_max_seq_length() #512
-        self.INDEX_PATH = self.config["paths"]["faiss_index"]
-        self.EMBED_PATH = self.config["paths"]["embeddings"]
+        self.INDEX_PATH = config["paths"]["faiss_index"]
+        self.EMBED_PATH = config["paths"]["embeddings"]
         self.index = None
         self.metadata = None
         
-        print(f"Created index instance:\n  embedding model: {self.config["models"]["embedding_model"]}\n  embedding size: {self.embed_size}\n  max sequence length: {self.max_seq_length}")
+        print(f"Created index instance:\n  embedding model: {config["models"]["embedding_model"]} on {self.device}\n  embedding size: {self.embed_size}\n  max sequence length: {self.max_seq_length}")
         
         #Reading index & metadata if created
         if os.path.exists(self.INDEX_PATH):
             self.index = faiss.read_index(self.INDEX_PATH)
             
-            with open(self.config["paths"]["faiss_metadata"], "rb") as f:
+            with open(config["paths"]["faiss_metadata"], "rb") as f:
                 self.metadata = pickle.load(f)   
             
-            print(f"Found and loaded:\n  index: {self.INDEX_PATH}\n  metadata: {self.config["paths"]["faiss_metadata"]}")
-        
+            print(f"Found and loaded:\n  index: {self.INDEX_PATH}\n  metadata: {config["paths"]["faiss_metadata"]}")
+        else:
+            print(f"No index/metadata file found.\n \
+                Check paths or build index")
+            
         print("_"*50)
-    
-    def _create_embeddings(self, texts):
-        print("Creating embeddings:")
-        
-        # Batch encode and append to H5
-        batch_size = 128
-        with h5py.File(self.EMBED_PATH, 'w') as hf:
-            dset = hf.create_dataset("embeddings", shape=(len(texts), self.embed_size), dtype='float32', chunks=True)
-            for i in tqdm(range(0, len(texts), batch_size)):
-                batch = texts[i:i + batch_size]
-                batch_emb = self.embed_model.encode(batch, batch_size=batch_size, show_progress_bar=False, precision='float32', normalize_embeddings=True)
-                dset[i:i + len(batch_emb)] = batch_emb
-                print(f"Batch {i//batch_size + 1} saved")
-        
-        print(f"Embeddings saved to {self.EMBED_PATH}")
-    
-    def build(self, gpu_enabled=False):
-        print("Building FAISS search index")
-        
-        DATA_PREP_PATH = self.config["paths"]["film_data"]
-        
-        #Check if the preprocessed data is present
-        if not os.path.exists(DATA_PREP_PATH):
-            print(f"ERROR: No preprocessed data found!\n  Run src/dataset/data_proc.py auto firstly.")
-            return
-            
-        data = pd.read_csv(DATA_PREP_PATH, usecols=["title", "title_plot", "title_meta"])
-        data = data.iloc[:300] #test_embeddings_build
-        data.reset_index(drop=True, inplace=True)
-        
-        #Creating text & metadata lists for index
-        texts = []
-        metadata = []
-        for idx, row in data.iterrows():
-            #Meta chunk
-            texts.append(row["title_meta"])
-            metadata.append({"row_idx": idx, "chunk_type": "meta", "title": row["title"], "chunk_text": row["title_meta"]})
-            
-            #Plot chunk
-            texts.append(row["title_plot"])
-            metadata.append({"row_idx": idx, "chunk_type": "plot", "title": row["title"], "chunk_text": row["title_plot"]})
-        
-        #Creating embeddings if not
-        if not os.path.exists(self.EMBED_PATH):
-            self._create_embeddings(texts)
-    
+
+    def build(self):
         #Loading embeddings
         print("Loading embeddings...")
-        with h5py.File(self.EMBED_PATH, 'r') as hf:
-            embeddings = hf['embeddings'][:].astype('float32')          
+        try:
+            with h5py.File(self.EMBED_PATH, 'r') as hf:
+                embeddings = hf['embeddings'][:].astype('float32') 
+        
+        except FileNotFoundError:
+            choice = input(f"No embeddings found {self.EMBED_PATH}. Ensure that file is present.\n \
+                Create embeddings? (y/n): ")
             
-        #Saving metadata
-        with open(self.config["paths"]["faiss_metadata"], 'wb') as f:
-            pickle.dump(metadata, f)
+            if choice.lower() == 'n':
+                print("Can't create index without embeddings")
+                return 
+            
+            else:
+                _create_embeddings(self.embed_model)
+                with h5py.File(self.EMBED_PATH, 'r') as hf:
+                    embeddings = hf['embeddings'][:].astype('float32')
+        
+        print(type(embeddings))
         
         #Creating an index
         num_clusters = int(np.sqrt(self.embed_size)) #XXX hyperparam to play with
@@ -94,48 +127,33 @@ class FaissIndex:
         index = faiss.IndexIVFFlat(quantizer, self.embed_size, num_clusters)
         
         #Training & adding & saving gpu/cpu index
-        if gpu_enabled:
-            try:
-                print("Moving index to GPU...")
-                resources = faiss.StandardGpuResources()
-                gpu_index = faiss.index_cpu_to_gpu(resources, 0, index)
-                
-                print("Training index on GPU...")
-                gpu_index.train(embeddings)
-                
-                print("Adding embeddings on GPU...")
-                gpu_index.add(embeddings)
-                
-                print("Saving CPU-version of index...")
-                cpu_index = faiss.index_gpu_to_cpu(gpu_index)
-                faiss.write_index(cpu_index, self.INDEX_PATH)        
-                print(f"Successfully built index!\n  chunks: {cpu_index.ntotal} ({cpu_index.ntotal//2} plots, {cpu_index.ntotal//2} film metadata)\n  saved to {self.INDEX_PATH}")
+        if torch.cuda.is_available():
+            print("Moving index to GPU...")
+            resources = faiss.StandardGpuResources()
+            gpu_index = faiss.index_cpu_to_gpu(resources, 0, index)
             
-            except Exception as e:
-                print(f"GPU indexing failed: {e}")
-                print("Falling back to CPU")
-                gpu_enabled = False
+            print("Training index on GPU...")
+            gpu_index.train(embeddings)
             
-        if not gpu_enabled:
+            print("Adding embeddings on GPU...")
+            gpu_index.add(embeddings)
             
+            index = faiss.index_gpu_to_cpu(gpu_index)
+            
+        else:
             print("Training index on CPU...") 
             index.train(embeddings)
             
             print("Adding Embeddings on CPU...")
             index.add(embeddings)
-            faiss.write_index(index, self.INDEX_PATH)
-            print(f"Successfully built index!\n  chunks: {index.ntotal} ({index.ntotal//2} plots, {index.ntotal//2} film metadata)\n  saved to {self.INDEX_PATH}")
+                   
+        print("Saving CPU-version of index...")
+        faiss.write_index(index, self.INDEX_PATH)        
+        print(f"Successfully built index!\n \
+            chunks: {index.ntotal} ({index.ntotal//2} plots, {index.ntotal//2} film metadata)\n  saved to {self.INDEX_PATH}")
+
         
-    def search(self, query:str, top_k:int):
-        #Checking if index accessible
-        if self.index == None:
-            if os.path.exists(self.INDEX_PATH):
-                self.index = faiss.read_index(self.INDEX_PATH)
-                with open(self.config["paths"]["faiss_metadata"], "rb") as f:
-                    self.metadata = pickle.load(f) 
-            else:
-                raise ValueError(f"ERROR: No faiss index found!")     
-        
+    def search(self, query:str, top_k:int):   
         embed_query = self.embed_model.encode([query], prompt_name="query", precision="float32", normalize_embeddings=True)
         self.index.nprobe = 10 #XXX hyperparam to play with
         
@@ -156,8 +174,6 @@ class FaissIndex:
 
 
 if __name__ == "__main__":
-    faiss_index = FaissIndex()
-    
     #Adding parser for different index functions
     parser = argparse.ArgumentParser(description="Parser for build/search functions")
     func_subparsers = parser.add_subparsers(dest="func", required=True, help="Choose function: build/search")
@@ -171,9 +187,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     if args.func == "build":
+        faiss_index = FaissIndex()
         faiss_index.build()
     
     elif args.func == "search":
+        faiss_index = FaissIndex()
         quit = False
         while not quit:
             query = input("Query: ")
